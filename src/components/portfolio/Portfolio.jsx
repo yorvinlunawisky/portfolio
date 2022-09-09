81dcb6d616185359f9b41b158b37e199cff50b52
yorvinlunawisky <yorvin0506@gmail.com>
2022-09-09 17:00:26 -0400

    Implementing sliders.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.webp';
+import IMG2 from '../../assets/portfolio2.webp';
+import IMG3 from '../../assets/portfolio3.webp';
+import IMG4 from '../../assets/portfolio4.webp';
+import IMG5 from '../../assets/portfolio5.webp';
+import IMG6 from '../../assets/portfolio6.webp';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  },
+  
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  },
+  
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  },
+  
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  },
+  
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  },
+  
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Crypto Currency DashBoard & Financial Visualization',
+    github: 'http://github.com',
+    demon: 'http://dribble.com'
+  }
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Porfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+                      
+            <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>              
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
 import './testimonials.css';
 
+import AVTR1 from '../../assets/avatar.jpg';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat maiores totam doloremque facilis animi aliquid accusamus sed deleniti. Soluta architecto voluptates odit omnis voluptas, aperiam asperiores molestias dolorem, facere, nam incidunt fugit! Reiciendis modi nisi deserunt, perspiciatis ipsam facilis deleniti!'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat maiores totam doloremque facilis animi aliquid accusamus sed deleniti. Soluta architecto voluptates odit omnis voluptas, aperiam asperiores molestias dolorem, facere, nam incidunt fugit! Reiciendis modi nisi deserunt, perspiciatis ipsam facilis deleniti!'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat maiores totam doloremque facilis animi aliquid accusamus sed deleniti. Soluta architecto voluptates odit omnis voluptas, aperiam asperiores molestias dolorem, facere, nam incidunt fugit! Reiciendis modi nisi deserunt, perspiciatis ipsam facilis deleniti!'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat maiores totam doloremque facilis animi aliquid accusamus sed deleniti. Soluta architecto voluptates odit omnis voluptas, aperiam asperiores molestias dolorem, facere, nam incidunt fugit! Reiciendis modi nisi deserunt, perspiciatis ipsam facilis deleniti!'
+  }
+];
+
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className='container testimonials__container'
+       // install Swiper modules
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review,}, index) => {
+            return (                      
+              <SwiperSlide key={index} className='testimonial'>
+                  <div className='client__avatar'>
+                    <img src={avatar}/>
+                  </div>
+                    <h5 className='client__name'>{name}</h5>
+                    <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
